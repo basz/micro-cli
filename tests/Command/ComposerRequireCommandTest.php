4e6d192c38155fef50c3182b8622749459310a5e
Bas Kamer <bas@bushbaby.nl>
2018-11-02 14:30:07 +0100

    apply prooph cs-fixer

--- tests/Command/ComposerRequireCommandTest.php
+++ tests/Command/ComposerRequireCommandTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the prooph/micro-cli.
+ * This file is part of prooph/micro-cli.
  * (c) 2017-2018 prooph software GmbH <contact@prooph.de>
  * (c) 2017-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
